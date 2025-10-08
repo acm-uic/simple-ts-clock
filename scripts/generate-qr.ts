e9ec12ecadf84326fd3c93aad50d5359f5702f46
Samuel Skean <sskean2@uic.edu>
2025-10-07 19:53:53 -0500

    Added logos to QR codes.
    A brief sojourn in the afternoon air cleared my head and showed me the
    way.
    Also I think I found a bug in `sharp`?

--- scripts/generate-qr.ts
+++ scripts/generate-qr.ts
@@ -1,17 +1,62 @@
 // samuel-skean: This was copied and pasted from ChatGPT, with minimal modifications. I have audited it, but this is also the first node code I've written.
-import QRCode from "qrcode";
+import QRCode, { toFile } from "qrcode";
+import sharp from "sharp";
 import { mkdirSync, readFileSync, rmSync } from "fs";
 import { resolve } from "path";
 import { qrCodeUrls } from "../src/qrCodeUrls";
 async function generate() {
-    const qrCodeDirectoryPath = resolve(__dirname, "../dist/public/qr/");
-    rmSync(qrCodeDirectoryPath, { recursive: true, force: true });
-    mkdirSync(qrCodeDirectoryPath);
-    for (const [ name, url ] of Object.entries(qrCodeUrls)) {
-        const file = resolve(__dirname, `../dist/public/qr/${name}.svg`);
-        QRCode.toFile(file, url, { type: "svg" });
-        console.log(`✅ QR code generated: ${file}`);
+  const qrCodeDirectoryPath = resolve(__dirname, "../dist/public/qr/");
+  rmSync(qrCodeDirectoryPath, { recursive: true, force: true });
+  mkdirSync(qrCodeDirectoryPath);
+    
+  const discordLogoFile = resolve(__dirname, "../assets/discord-icon-svgrepo-com.svg");
+  const discordLogo = sharp(discordLogoFile);
+  const { width: discordLogoWidth, height: discordLogoHeight } = await discordLogo.metadata();
+  console.log(discordLogoHeight);
+  
+  for (const [name, url] of Object.entries(qrCodeUrls)) {
+    // TODONOW(samuel-skean): Remove all intermediate file steps!
+    const qrCodeFile = resolve(__dirname, `../dist/public/qr/${name}.png`);
+    if (name === "appRepo") {
+      await QRCode.toFile(qrCodeFile, url, { type: "png", errorCorrectionLevel: "H" });
+      console.log(`✅ App Repo QR code generated: ${qrCodeFile}`);
+      continue;
     }
+    const qrCode = sharp(await QRCode.toBuffer(url, { type: "png", errorCorrectionLevel: "H" }));
+    const { width: originalQrCodeWidth, height: originalQrCodeHeight } = await qrCode.metadata();
+    
+    
+    // For who knows what reason, the "qrcode" library generates images where each "QR code pixel" is 4 x 4 pixels of the image. So, first we resize it down.
+    const [qrCodeWidth, qrCodeHeight] = [originalQrCodeWidth / 4, originalQrCodeHeight / 4];
+    const qrCodeWithGap = await qrCode
+      .resize({ width: qrCodeWidth, kernel: sharp.kernel.nearest })
+      .composite(
+        [
+          {
+            input: {
+              create: {
+                width: Math.ceil(qrCodeWidth / 4),
+                height: Math.ceil(qrCodeHeight / 4),
+                channels: 4,
+                background: "white"
+              }
+            }
+          }, 
+        ])
+      .toBuffer();
+    await sharp(qrCodeWithGap)
+      .resize({ width: 5 * discordLogoWidth, kernel: sharp.kernel.nearest })
+      .composite(
+        [
+          {
+            input: await discordLogo.grayscale().threshold(200).toBuffer(),
+          }
+        ]
+      )
+      .png()
+      .toFile(qrCodeFile);
+    console.log(`✅ Discord QR code generated: ${qrCodeFile}`);
+  }
 }
 
 generate().catch((err) => {
