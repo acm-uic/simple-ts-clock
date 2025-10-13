087676b892bd9e44f96523dd09f73a7ef2d98de4
Samuel Skean <sskean2@uic.edu>
2025-10-13 09:22:37 -0500

    Upgrade nodejs, and version of nixpkgs more broadly.
    Doesn't change the configuration of the machine, just how this app is
    built.
    Enables using vite.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "ACMClock Packaging and Deployment";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     agenix.url = "github:ryantm/agenix";
   };
 
